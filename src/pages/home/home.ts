18d98a950f3cb1d17fef505e5bedcb9f14f28a90
2ez4salt <talhaslt@gmail.com>
2019-07-07 17:46:36 +0300

    project initilaized

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  ilkDegiskenimiz:string  = "Staj Okul";
+  gorevListesi = [];
+  arr = [1,2,3,4,5];
+  task;
+  constructor(public navCtrl: NavController,public alertCtrl:AlertController) {}
+  
+  girisAction(kullancıAdı,sifre) {
+    console.log("Kullanıcı Adınız : " + kullancıAdı)
+    console.log("Şifreniz : " + sifre)
+    const bizimAlertımız = this.alertCtrl.create({
+      title: 'Hoşgeldin Stajyer',
+      subTitle: 'Kullanıcı Adınız : ' + kullancıAdı +  '<br>'+ ' Şifreniz : ' + sifre,
+      buttons: ['OK'],
+      enableBackdropDismiss : false
+    });
+    bizimAlertımız.present()  
   }
 
+  addTask(gorev){
+    this.gorevListesi.push(gorev)
+    console.log(this.gorevListesi);
+    this.task = '';
+  }
+
+
+  
 }
